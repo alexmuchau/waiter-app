0aed094d9fc3dd8d51260e0a7c5ecd9b7f1679bc
Alex Muchau <muchau04@gmail.com>
2024-09-17 19:07:30 -0300

    fix: :zap: Adjusting app.ts error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import createServer from "./server";
 const port = 4100;
 const app = createServer();
 
-app.listen({ host: "0.0.0.0", port }, (err: Error) => {
+app.listen({ host: "0.0.0.0", port }, (err: Error | null, address: string) => {
   if (err) {
     console.log(err);
   }
