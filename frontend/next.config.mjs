26b5961b29bb0a79e51500e9fda875271b05d500
Alex Muchau <muchau04@gmail.com>
2024-09-21 11:43:10 -0300

    feat(FRONT): :rocket: Deploying base front structure

---
+++ frontend/next.config.mjs
@@ -0,0 +1,16 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+  cacheOnFrontEndNav: true,
+  workboxOptions: {
+    disableDevLogs: true
+  }
+});
+
+export default withPWA({
+  // Your Next.js config
+});
