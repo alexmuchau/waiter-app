bd2e54cb97d64689ca26ed43ef630780f7ac37c1
Alex Muchau <muchau04@gmail.com>
2025-04-02 08:45:58 -0300

    fix: :bug: Fix HOST backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,21 +2,23 @@ import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
 import { fastifyJwt } from "@fastify/jwt"
 
-import { authMiddleware } from './middleware';
 import { routes } from './routes/routes';
 import { syncDBs } from './tools/syncDBs';
+
+const HOST = process.env.HOST ?? 'localhost'
+const PORT = 8080
+const JWT_SECRET = process.env.JWT_SECRET ?? 'secret'
 
 const app = fastify()
 
 app.register(fastifyCors, { origin: '*' })
 app.register(fastifyJwt, {
-    secret: process.env.JWT_SECRET ?? 'secret'
+    secret: JWT_SECRET
 });
 
 app.register(routes)
 
-app.listen({ host: process.env.HOST, port: 8080 }).then( async () => {
+app.listen({ host: HOST, port: PORT }).then( async () => {
     await syncDBs()
-    
-    console.log('Server running')
+    console.log(`Server running on http://${HOST}:${PORT}`)
 })
